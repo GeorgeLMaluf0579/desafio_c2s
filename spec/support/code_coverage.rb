ccf5f1640dd8f54f6ccb0963592122c1e3c0f9c2
George Luiz 'Mav' Maluf <georgelmaluf286@gmail.com>
2025-10-29 12:11:09 -0300

    feat: config rspec to run suit tests

---
+++ spec/support/code_coverage.rb
@@ -0,0 +1,10 @@
+require 'simplecov'
+
+SimpleCov.start 'rails' do
+  add_filter '/bin'
+  add_filter '/db'
+  add_filter '/spec'
+  
+  add_group 'Models', 'app/models'
+  add_group 'Controllers', 'app/controllers'
+end

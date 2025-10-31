19aee3a488edc9106a375c974f60d0b2f6391879
George Luiz 'Mav' Maluf <georgelmaluf286@gmail.com>
2025-10-31 18:35:32 -0300

    feat: Add EmailProcessorService, parsers and tests

---
+++ app/services/parsers/fornecedor_a.rb
@@ -0,0 +1,20 @@
+module Parsers
+  class FornecedorA < Base
+    PATTERN_KEYS = {
+      name: ['Nome do cliente', 'Nome'],
+      email: ['E-mail', 'Email'],
+      phone: ['Telefone'],
+      product_code: []
+    }.freeze
+    
+    def parse!
+      data = extract_pattern(PATTERN_KEYS)
+
+      product_match = @body.match(/produto de código\s*([A-Z0-9\-]+)/i) || 
+                      @body.match(/produto\s*([A-Z0-9\-]+)/i)
+      data[:product_code] = product_match[1] if product_match
+      data[:subject] = @email.subject
+      data
+    end
+  end
+end
---
+++ app/services/parsers/parceiro_b.rb
@@ -0,0 +1,17 @@
+module Parsers
+  class ParceiroB < Base
+    PATTERN_KEYS = {
+      name: ['Nome completo', 'Nome do cliente', 'Cliente'],
+      email: ['E-mail de contato', 'Email'],
+      phone: ['Telefone'],
+      product_code: ['Produto de interesse', 'Código do produto', 'Produto']
+    }.freeze
+
+    def parse!
+      data = extract_pattern(PATTERN_KEYS)
+
+      data[:subject] = @email.subject
+      data
+    end
+  end
+end

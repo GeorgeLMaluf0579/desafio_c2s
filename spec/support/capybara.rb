87085577b5493ddac29bbe2fa1b57552a2a2639b
George Luiz 'Mav' Maluf <georgelmaluf286@gmail.com>
2025-11-01 18:47:16 -0300

    chore: removed unnecessary spec files

--- spec/support/capybara.rb
+++
@@ -1,6 +0,0 @@
-require 'capybara/rspec'
-
-RSpec.configure do |config|
-  config.include Capybara::DSL
-  config.include Capybara::RSpecMatchers
-end
--- spec/system/home/index.html.erb_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe 'Home Index Page', type: :system do
-  it 'show the Home index page heading' do
-    visit root_path
-    expect(page).to have_content('Home')
-  end
-end
